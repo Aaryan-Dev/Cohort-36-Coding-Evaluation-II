89bbf9a2c9510a5172bed5f585ada9b2855b59b0
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-04-26 00:57:09 +0530

    Update Pattern Printing V __REVISIT___.js

--- Pattern Printing V __REVISIT___.js
+++ Pattern Printing V __REVISIT___.js
@@ -1,3 +1,28 @@
 // Given an integerN, and a pattern. Print the pattern as given in the sample I/O.
 
 // Note: Please check for spaces.
+
+function patternPrinting(N) {
+    
+    for(i=0; i<N; i++){
+        bag = "";
+        
+        for(j=0; j<N*2-1; j++){
+            
+            if(j%2==0){
+                
+                if(j<N*2-1-(i*2)){
+           bag = bag + "*";
+                }else{
+                  bag = bag + " ";  
+                }
+           
+            }else{
+                bag = bag + " "; 
+            }
+            
+        }
+        console.log(bag);
+          
+}
+}
